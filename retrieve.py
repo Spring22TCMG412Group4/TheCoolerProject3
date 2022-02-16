3310093a2ef07b863ca9311522aad042d2342554
pmott32 <98490734+pmott32@users.noreply.github.com>
2022-02-16 15:00:07 -0600

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -5,3 +5,6 @@ LOCAL_FILE = 'local_copy.log'
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+if os.path.isfile('local_copy.log'):
+        with open('local_copy.log', 'r') as http_info:
